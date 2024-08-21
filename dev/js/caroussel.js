f16e119368d486461931ec7ed9b194d06ac5f247
Eric Njanga <eric.njanga@gmail.com>
2024-08-21 16:21:08 -0400

    Simplifying the whole layout (phase 7)

--- dev/js/caroussel.js
+++ dev/js/caroussel.js
@@ -35,5 +35,59 @@ export function setupCarousselFct() {
           },
         ],
       });
+
+
+
+      /**
+       * TODO:
+       * ORGANIZE THIS INTO A NICE FUNCTION
+       */
+
+      // Cache selectors for performance
+      var $prevButton = $('.btn-carousel-prev');
+      var $nextButton = $('.btn-carousel-next');
+      var $carousel = $('#enjanga-caroussel-1');
+
+      // Initial check to disable/enable buttons
+      updateButtons();
+
+      // Bind custom buttons to Slick methods
+      $prevButton.on('click', function(){
+        $carousel.slick('slickPrev');
+      });
+
+      $nextButton.on('click', function(){
+        $carousel.slick('slickNext');
+      });
+
+      // Listen to the afterChange event
+      $carousel.on('afterChange', function(event, slick, currentSlide){
+        updateButtons();
+      });
+
+      // Function to enable/disable buttons
+      function updateButtons() {
+        var slick = $carousel.slick('getSlick');
+        var currentSlide = slick.currentSlide;
+        var slideCount = slick.slideCount;
+
+        // Disable the "Prev" button if on the first slide
+        if (currentSlide === 0) {
+          $prevButton.prop('disabled', true);
+        } else {
+          $prevButton.prop('disabled', false);
+        }
+
+        // Disable the "Next" button if on the last slide
+        if (currentSlide === slideCount - 1) {
+          $nextButton.prop('disabled', true);
+        } else {
+          $nextButton.prop('disabled', false);
+        }
+      } 
+
+
+
+
   }
 }
--- dev/js/main.js
+++ dev/js/main.js
@@ -35,12 +35,29 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 /**
+ * Automatically collapse the Bootstrap navbar when a nav item is clicked on mobile devices
  * TODO:
  * -- PUT THIS IN ITS OWN FILE WHENEVER POSSIBLE
  */
-document.addEventListener('DOMContentLoaded', function () {
+$(document).ready(function() {
+  // Select all nav items
+  $('.navbar-collapse .nav-item').on('click', function() {
+    // Check if the navbar is expanded (visible)
+    var $navbar = $('.navbar-collapse');
+    if ($navbar.hasClass('show')) {
+      // Collapse the navbar
+      $navbar.collapse('hide');
+    }
+  });
+});
 
-  // This function adds/removes a class to the nav when the page scrolls past a threshold
+
+/**
+ * This function adds/removes a class to the nav when the page scrolls past a threshold
+ * TODO: 
+ * -- PUT THIS IN ITS OWN FILE WHENEVER POSSIBLE
+ */
+document.addEventListener('DOMContentLoaded', function () { 
   const navigation = document.querySelector('#main-nav'); // Change 'nav' to your navigation element selector
   const threshold = 100; // Set your desired scroll threshold in pixels
   const activeClass = 'navbar-masked'; // Set the class to add/remove
