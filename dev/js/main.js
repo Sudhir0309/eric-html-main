ca8466e3ab2b1f94677ae189e36ed9ef97da50b5
Eric Njanga <eric.njanga@gmail.com>
2024-08-21 16:37:53 -0400

    Simplifying the whole layout (phase 8)

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,7 +1,5 @@
 'use strict';
 
-import { handleViewportChange as handleViewportChange } from './videoBannerAccessibility.js';
-import { setElementSticky as setElementSticky } from './sticky-nav.js';
 import { setupScrolltopBtn as setupScrolltopBtn } from './scroll-top-btn.js';
 import { setupChatbotBtn as setupChatbotBtn } from './chatbot.js'
 import { setupCarousselFct as setupCarousselFct } from './caroussel.js'
@@ -9,20 +7,6 @@ import { setupCarousselFct as setupCarousselFct } from './caroussel.js'
 
 
 document.addEventListener('DOMContentLoaded', function () {
-  const mediaQuery = window.matchMedia('(min-width: 992px)');
-
-  /**
-   * Hide/reveal DOM elements to accessible technologies based on media queries
-   */
-  // 1) Initial check
-  handleViewportChange(mediaQuery);  
-  // 2) Listen for changes in the viewport width
-  mediaQuery.addListener(handleViewportChange);
-
-
-  // Setting up sticky nav ...
-  // setElementSticky('main-nav');
-
   // Setting up scrolltop button ...
   setupScrolltopBtn('scroll-top-button', 300);
 
